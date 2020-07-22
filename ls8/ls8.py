4e128c9cb2895821f262109687dc72d092db3630
Robin Srimal <robin.srimal@gmail.com>
2020-07-22 22:59:25 +0200

    day 2

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -7,5 +7,20 @@ from cpu import *
 
 cpu = CPU()
 
-cpu.load()
+if len(sys.argv) != 2:
+    print("Please provide a program file!")
+
+ls8_re = re.compile(r"^(?P<byte>[01]{8})?(\s*#(?P<comment>.*))?")
+
+filename = sys.argv[1]
+program = []
+
+with open(filename, "r") as f:
+
+    for line in f.readlines():
+        m = ls8_re.match(line)
+        if m and m.group("byte"):
+            program.append(int(m.group("byte"), 2))
+
+cpu.load(program)
 cpu.run()
